8825bec841e760bae5b7c9ab7aaeebcd67da0362
Prathameshdarekar <161707207+Prathameshdarekar@users.noreply.github.com>
2024-03-01 12:32:45 +0530

    Welcome_message.cs

--- Welcome_message.cs
+++ Welcome_message.cs
@@ -7,7 +7,6 @@ namespace CSharpPractice
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to C# Practice!");
-            Console.WriteLine("This repository contains exercises and small projects to help you learn C#.");
             Console.WriteLine("Feel free to explore and experiment with the code!");
         }
     }
